f1aa848a810d95632fcce876c95240e57e1a6a20
Shane Quigley <quigles@gmail.com>
2026-02-15 21:44:27 +0000

    test(drag-drop): add SQLite repository tests for coverage

--- src/infrastructure/sqlite/index.ts
+++ src/infrastructure/sqlite/index.ts
@@ -1,4 +1,5 @@
 export { createConnection } from './connection.js';
+export { SqliteComponentPositionRepository } from './component-position-repository.js';
 export { SqliteEdgeRepository } from './edge-repository.js';
 export { SqliteFeatureRepository } from './feature-repository.js';
 export { SqliteNodeRepository } from './node-repository.js';
